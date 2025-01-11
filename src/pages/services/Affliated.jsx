b8c42afcfa7dfa6d7d60ad17745ad63a8295bb01
bhaskarAntony <rooparoopa7105@gmail.com>
2025-01-11 18:49:55 +0530

    all files

--- src/pages/services/Affliated.jsx
+++ src/pages/services/Affliated.jsx
@@ -4,6 +4,7 @@ import image2 from '../../assets/Branding/Screenshot 2025-01-09 135011.png'
 import image3 from '../../assets/Branding/tv19_logo final.png'
 import image4 from '../../assets/Branding/IMG_0218.png'
 import image5 from '../../assets/Branding/IMG_1862.png'
+import image6 from '../../assets/Branding/mr d new.png'
 
 import i1 from '../../assets/Branding/affliated.png'
 import i2 from '../../assets/Branding/affliated1.png'
@@ -30,6 +31,10 @@ function Affliated() {
         {
             image:image5,
             title:'First Day First Show'
+        },
+        {
+            image:image6,
+            title:'Mr D'
         }
     ]
   return (
@@ -50,7 +55,7 @@ function Affliated() {
         }
     </div>
 
-    <h1 className="fs-1 fw-bold text-center text-white mb-4">Affliated <span className="text-aqua">Marketing</span></h1>
+    <h1 className="fs-1 fw-bold text-center text-white mb-4">Affliated <span className="text-aqua">Marketing Pages</span></h1>
     <div className="line"></div>
     <div className="row">
         <div className="col-md-4 instagram d-flex align-items-center justify-content-center rounded-3">
