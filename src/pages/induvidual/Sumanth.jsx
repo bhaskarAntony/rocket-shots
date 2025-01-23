e4404a1365ed8cb2847e2f0edbe57ba6c1684be9
bhaskarAntony <rooparoopa7105@gmail.com>
2025-01-24 01:45:55 +0530

    all files

--- src/pages/induvidual/Sumanth.jsx
+++ src/pages/induvidual/Sumanth.jsx
@@ -5,7 +5,7 @@ import About from './About'
 function Sumanth() {
   return (
     <section className="container-fluid p-0">
-        <Hero title="SUMANTH RAM" subtitle="Govind is a highly creative video editor with an exceptional talent for transforming ordinary footage into extraordinary content through his innovative ideas and unique editing styles. With a mechanical background, he discovered his passion for filmmaking and honed his skills in presenting diverse content creatively, tailored to various fields. His ability to bring fresh perspectives and artistic enhancements makes him a valuable asset to any project. Whether on set or in the editing room, Govind’s expertise consistently elevates the quality and impact of the final product." image={sumanth}/>
+        <Hero title="SUMANTH RAM" subtitle="Sumanth is a highly creative and dedicated filmmaker with a Master's degree in Theatre Arts. His extraordinary skills as a screenwriter, particularly in creating captivating screenplays, are fuelled by his love of movies. In addition, he is a talented director who knows how to bring stage and film productions to life with accuracy and vision. Sumanth is a great asset to any movie production because of his innovative ideas and commitment to the craft of filmmaking." image={sumanth}/>
         </section>
   )
 }
